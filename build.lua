1736dbe7dde08e8c0f38012d88a8d791ae53640b
Adrian Pistol <adinator1999@gmail.com>
2015-02-10 19:00:44 +0100

    Fixed wrong env variable

--- build.lua
+++ build.lua
@@ -7,7 +7,7 @@ end
 local files = table.concat({"cllua.cpp"}," ")
 local obj = "cllua.o"
 local exe = "cllua"
-local cpp = os.getenv("CPP") or "g++"
+local cpp = os.getenv("CXX") or "g++"
 local libs = "-l"..table.concat({"OpenCL","lua5.1"}," -l")
 local cppflags = "-c -std=c++11"
 
